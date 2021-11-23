a0d95d6cc3257db1b30b47c7d5d341a75043a617
martinbd <78870498+UO276244@users.noreply.github.com>
2021-11-23 10:27:40 +0100

    Add files via upload

---
+++ Ejercicio-10/Ejercicio10.js
@@ -0,0 +1,249 @@
+
+
+
+class Electricidad{
+
+   
+
+    constructor(initFecha, endFecha, lan, categoria, widget, hsDay){                                                              
+        
+        this.truncday = "";
+        if(hsDay){
+            this.truncday = "&time_trunc=day";
+        }
+        this.startDate = "start_date=" + initFecha + "T00:00";
+        this.endDate = "end_date=" + endFecha + "T23:59";
+        this.ccaa = "geo_limit=" + "peninsular"; //alcance = ccaa o bien peninsular
+        this.category = categoria;
+        this.widget = widget;
+        this.lang = lan;
+        this.query = "" + this.startDate + "&" + this.endDate;
+        this.url = "https://apidatos.ree.es/" + this.lang + "/datos/" + this.category + "/" + this.widget + "?" + this.query + this.truncday + "&" + this.ccaa;
+        this.datos = null;
+        
+    }
+
+
+    cargarDatos(filename, request){
+
+       
+
+        $.ajax({
+            dataType: "json",
+            url: this.url,
+            method: 'GET',
+            success: function(datos){
+
+               request.download(filename, JSON.stringify(datos,null,2));
+                },
+
+            error:function(){
+                alert("Oh oh, algo no ha ido como se esperaba, quizás no haya datos en la plataforma,Repase los datos introducidos o pruebe a cambiar la seleccion de truncado por día.");
+               return;
+                
+                }
+          
+        });
+
+    }
+
+
+    currDate(){
+       let n =  new Date();
+        let y = n.getFullYear();
+        let m = n.getMonth();
+        let d = n.getDate() - 1;
+
+        return "" + y + "-" + m + "-" + d;
+    }
+
+
+    
+
+}
+
+
+class RequestCreator{
+
+
+    constructor(){
+        this.widgetsBalance = ["balance-electrico"];
+
+        this.widgetsDemanda = ["evolucion"
+        ,"ire-general-movil","ire-industria","ire-industria-movil",
+        "ire-servicios","ire-servicios-movil"];
+
+
+        this.widgetsGeneracion = ["estructura-generacion","evolucion-renovable-no-renovable","estructura-renovables",
+        "estructura-generacion-emisiones-asociadas","evolucion-estructura-generacion-emisiones-asociadas","no-renovables-detalle-emisiones-CO2",
+        "maxima-renovable", "potencia-instalada", "maxima-renovable-historico", "maxima-sin-emisiones-historico"];
+    
+        this.widgetsIntercambios = ["francia-frontera", "portugal-frontera", "marruecos-frontera",
+        "andorra-frontera", "lineas-francia", "lineas-portugal", "lineas-marruecos","lineas-andorra","francia-frontera-programado" ,
+        "portugal-frontera-programado", "marruecos-frontera-programado", "andorra-frontera-programado", "enlace-baleares",
+        "frontera-fisicos","todas-fronteras-fisicos", "frontera-programados", "todas-fronteras-programados"];
+            
+
+        this.widgetsTrasnporte =["energia-no-suministrada-ens", "indice-indisponibilidad", "tiempo-interrupcion-medio-tim", "kilometros-lineas",
+    "indice-disponibilidad", "numero-cortes","ens-tim","indice-disponibilidad-total"];
+      
+        this.widgetsMercados = ["componentes-precio-energia-cierre-desglose", "componentes-precio", "energia-gestionada-servicios-ajuste",
+        "energia-restricciones","precios-restricciones","reserva-potencia-adicional","banda-regulacion-secundaria",
+        "energia-precios-regulacion-secundaria","energia-precios-regulacion-terciaria","energia-precios-gestion-desvios",
+        "coste-servicios-ajuste","volumen-energia-servicios-ajuste-variacion","precios-mercados-tiempo-real",
+        "energia-precios-ponderados-gestion-desvios-before","energia-precios-ponderados-gestion-desvios","energia-precios-ponderados-gestion-desvios-after"];
+    }
+
+    pedirElectricista(initFecha, endFecha, lan, categoria, widget,hasDay){
+
+        return new Electricidad(initFecha, endFecha, lan, categoria, widget,hasDay);
+
+
+    }
+
+    comprobarCampos(){
+
+        if($("#selecFechaInit").val() > $("#selecFechaFin").val()){
+            alert("Las fecha de inicio es posterior a la fecha de fin.");
+            return;
+        }
+
+        let haySeleccionada = false;
+       
+        let i = 1;
+        for(i = 1; i <= 6; i++){
+            if($('#categoria' + i).is(":checked")){
+                haySeleccionada = true;
+
+                
+                return $('#categoria' + i).val();
+            }
+        }
+        
+
+        if(haySeleccionada == false){
+            alert("Seleccione una categoría");
+            return;
+        }
+
+
+    }
+
+    
+    download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+      
+        element.style.display = 'none';
+        document.body.appendChild(element);
+      
+        element.click();
+      
+        document.body.removeChild(element);
+    }
+
+
+
+    writeWidgets(array){
+
+        let str = "";
+
+            let i = 0;
+            for(i = 0; i < array.length; i++){
+
+                str += "<option value=\"" + array[i] + "\">" + array[i] + "</option>";
+
+            }
+            $("#nombreArchivo:first").before("<select name=\"select\">" + str + "</select>");
+
+
+    }
+
+    loadWidgets(categoria){
+
+        $("select").remove();
+
+        if(categoria == 'balance'){
+
+
+            this.writeWidgets(this.widgetsBalance);
+
+
+
+        }else if(categoria == 'demanda'){
+
+            this.writeWidgets(this.widgetsDemanda);
+
+
+
+        }else if(categoria == 'generacion'){
+
+            this.writeWidgets(this.widgetsGeneracion);
+
+        }else if(categoria == 'intercambios'){
+
+            this.writeWidgets(this.widgetsIntercambios);
+
+        }else if(categoria == 'transporte'){
+
+            this.writeWidgets(this.widgetsTrasnporte);
+
+        }else { //Mercados
+            this.writeWidgets(this.widgetsMercados);
+        }
+
+    }
+
+
+
+    iniciarRequest(){
+
+
+       let categoriaStr =  this.comprobarCampos();
+
+       if(categoriaStr==''){
+            return;
+       }
+        
+        
+       let hasDay = false;
+
+       var checkbox = document.getElementById('porDia');
+
+       if(checkbox.checked){
+        hasDay = true;
+       }
+
+        let electricista = this.pedirElectricista($("#selecFechaInit").val() , $("#selecFechaFin").val(), "es", categoriaStr, $("select option:selected").val(),hasDay);
+
+        electricista.cargarDatos($('input[type=text]').val(),this);
+      
+      
+
+        
+       
+       
+
+       
+        
+    }
+
+    checkearCheckbox(categoria){
+
+        
+       
+        $('input:checkbox').not($("#"+categoria)).prop('checked', false);
+        
+
+        let cat = $("#"+categoria).val();
+        this.loadWidgets(cat);
+
+    }
+
+}
+
+
+var requestCreator = new RequestCreator();
+
+
